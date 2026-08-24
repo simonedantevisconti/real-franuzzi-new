3bfcbd09b1ca1e3fd7386c853c230e81a6cce768
Simone Visconti <simo.visco4@gmail.com>
2026-08-24 17:26:50 +0200

    [UPD] scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 
 import DefaultLayout from "./layouts/DefaultLayout";
 
+import ScrollToTop from "./components/ScrollToTop";
+
 import Homepage from "./pages/Homepage";
 import Rosa from "./pages/Rosa";
 import Calendario from "./pages/Calendario";
@@ -10,15 +12,19 @@ import Contatti from "./pages/Contatti";
 
 const App = () => {
   return (
-    <Routes>
-      <Route element={<DefaultLayout />}>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/rosa" element={<Rosa />} />
-        <Route path="/calendario" element={<Calendario />} />
-        <Route path="/statistiche" element={<Statistiche />} />
-        <Route path="/contatti" element={<Contatti />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/rosa" element={<Rosa />} />
+          <Route path="/calendario" element={<Calendario />} />
+          <Route path="/statistiche" element={<Statistiche />} />
+          <Route path="/contatti" element={<Contatti />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
